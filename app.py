4d461c79beac2338380f1695af16d5ddd05956b1
Stanley Zheng <dev.stanzheng@gmail.com>
2016-10-08 17:28:52 -0400

    clean up into a class

--- app.py
+++ app.py
@@ -2,83 +2,91 @@
 from collections import defaultdict
 import re
 
-state = 'oslc elai tant myse'
-wordlist = 'bsd_wordlist.txt'
-# state = 'to fu'
-# wordlist = 'testwords.txt'
-LETTERS = state.replace(' ', '')
-LETTER_SET = set(i for i in LETTERS)
-COL_LEN = int(len(LETTERS)**(.5))
-MIN_LETTERS = 3
-def grid(b):
-    return b.split(' ')
 
-def matrix():
-    return [[l for l in i] for i in grid(state)]
+class Boggle():
 
-def breakdown_word(word):
-    """Use string slices to get all combos of a word"""
-    return [word[:i+1]for i in range(len(word))]
+    def __init__(self, state, wordlist):
 
-def load_words():
-    """"""
-    d = defaultdict(set)
-    with open(wordlist) as words:
-        for word in words:
-            word = word.lower().rstrip('\n')
-            if len(word) <= len(LETTERS) and len(word) >= MIN_LETTERS:
-               for k in breakdown_word(word):
-                   d[k].add(word)
-        return d
+        self.state = state
+        # CONSTANTS
+        self.MOVESETS = [(1,0), (0,1), (-1,0), (0,-1), (-1,-1), (1,1), (-1,1), (1,-1)]
+        self.MIN_LETTERS = 3
+        self.LETTERS = state.replace(' ', '')
+        self.LETTER_SET = set(i for i in self.LETTERS)
+        self.COL_LEN = int(len(self.LETTERS)**(.5))
 
-def check_valid(move):
-    return True if move[0] >= 0 and move[0] < COL_LEN and move[1] >= 0 and move[1] < COL_LEN else False
+        # SETUP GAME OBJECTS
+        self.m = self.matrix()
+        self.word_set = self.load_words(wordlist)
+        self.onboard = set()
 
-def add_sets(a, b):
-    return (a[0]+b[0], a[1]+b[1])
+        # Solve Board
+        for x, row in enumerate(self.grid(state)):
+            for y, letter in enumerate(row):
+                print(x,y,letter)
+                self.traverse(x,y)
 
-m = matrix()
-onboard = set()
-movesets = [(1,0), (0,1), (-1,0), (0,-1), (-1,-1), (1,1), (-1,1), (1,-1)]
-word_set = load_words()
+    def add_sets(self, a, b):
+        return (a[0]+b[0], a[1]+b[1])
 
-def xy_to_words(coordinates):
-    word = ''
-    for coord in coordinates:
-        word += m[coord[0]][coord[1]]
-    return word
+    def breakdown_word(self, word):
+        """Use string slices to get all combos of a word"""
+        return [word[:i+1]for i in range(len(word))]
 
-def traverse(x, y, visited=None):
-    letter = m[x][y]
-    origin = (x,y)
-    if visited is None:
-        # to create a new visited param after first initialized
-        visited = [origin]
-    next_move = [add_sets(origin, n) for n in movesets if check_valid(add_sets(origin, n))]
-    # print(origin, next_move)
-    for move in next_move:
-        # simport ipdb; ipdb.set_trace()
-        if (move not in visited):
-            path = visited + [move]
-            # check next word if possible, if so continue
-            possibility = xy_to_words(path)
-            if possibility in word_set:
-                if possibility in word_set.get(possibility):
-                    onboard.add(xy_to_words(path))
-                traverse(move[0], move[1], path)
+    def check_valid(self, move):
+        return True if move[0] >= 0 and move[0] < self.COL_LEN and move[1] >= 0 and move[1] < self.COL_LEN else False
 
+    def grid(self, rows):
+        return rows.split(' ')
 
-if __name__ == '__main__':
+    def load_words(self, wordlist):
+        """"""
+        d = defaultdict(set)
+        with open(wordlist) as words:
+            for word in words:
+                word = word.lower().rstrip('\n')
+                if len(word) <= len(self.LETTERS) and len(word) >= self.MIN_LETTERS:
+                   for k in self.breakdown_word(word):
+                       d[k].add(word)
+            return d
+
+    def matrix(self):
+        return [[l for l in i] for i in self.grid(self.state)]
+
+    def traverse(self, x, y, visited=None):
+        origin = (x,y)
+        if visited is None:
+            # to create a new visited param after first initialized
+            visited = [origin]
+
+        # Get valid Move List
+        next_move = [self.add_sets(origin, n) for n in self.MOVESETS if self.check_valid(self.add_sets(origin, n))]
+        for move in next_move:
+            if (move not in visited):
+                path = visited + [move]
+                # check next word if possible, if so continue
+                possibility = self.xy_to_words(path)
+                if possibility in self.word_set:
+                    if possibility in self.word_set.get(possibility):
+                        self.onboard.add(self.xy_to_words(path))
+                    self.traverse(move[0], move[1], path)
+
+    def xy_to_words(self, coordinates):
+        word = ''
+        for coord in coordinates:
+            word += self.m[coord[0]][coord[1]]
+        return word
+
 # def main():
-    possibilities = {}
-    # Build word possibilities
-    for x, row in enumerate(grid(state)):
-        for y, letter in enumerate(row):
-            print(x,y,letter)
-            traverse(x,y)
-            # traverse(x,y, visited)
+if __name__ == '__main__':
+    state = 'oslc elai tant myse'
+    wordlist = 'bsd_wordlist.txt'
+    # state = 'to fu'
+    # wordlist = 'testwords.txt'
 
-    print(len(onboard))
+    board = Boggle(state, wordlist)
+
+    print('num words on the board:', len(board.onboard))
 
 # if __name__ == '__main__':
 #     main()
